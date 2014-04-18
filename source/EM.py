c7ab15d18daa1b24cda40dd8bf320aef5ee361b7
Zach Welch <zwelch@wisc.edu>
2014-04-17 23:51:47 -0500

    Pretty much done; need to add log likelihood and run some more tests

--- source/EM.py
+++ source/EM.py
@@ -3,8 +3,8 @@ import numpy as np
 import math
 
 
-def Normal(Mu,Sigma,X):
-    math.exp(-.5*(x-Mu)*Sigma.I*(x-Mu).T)/sqrt((2*math.pi)**len(X)*np.linalg.det(Sigma))    
+def Normal(Mu,Sigma,x):
+    return math.exp(-.5*(x-Mu)*Sigma.I*(x-Mu).T)/math.sqrt((2*math.pi)**len(x)*np.linalg.det(Sigma))    
 
 def EM(L,U,Wl,Wu,Ys,maxIter,threshold):
     #L and U are lists of tuples L being labeled data and U being unlabeled data.
@@ -14,77 +14,107 @@ def EM(L,U,Wl,Wu,Ys,maxIter,threshold):
     
     #Init using only labeled data
     Ycounts  = [0.0,0.0]
-
+    Wlcounts = [0.0,0.0]
     for l in L:
-        Ycounts[Ys.find(l[1])]= Ycounts[Ys.find(l[1])]+1
-
+        Ycounts[Ys.index(l[1])]= Ycounts[Ys.index(l[1])]+1
+        Wlcounts[Ys.index(l[1])]=Wlcounts[Ys.index(l[1])]+Wl[L.index(l)]
 
     Mus    = []
     Sigmas = []
     for yNdx in range(len(Ys)):
-        Mu = mat(zeros(len(L[0][0].keys())))
-        Sigma=mat(zeros([len(L[0][0].keys()),len(L[0][0].keys())]))
+        effW = Ycounts[yNdx]/Wlcounts[yNdx]
+        Mu = np.mat(np.zeros(len(L[0][0].keys())))
+        Sigma=np.mat(np.zeros([len(L[0][0].keys()),len(L[0][0].keys())]))
         for lNdx in range(len(L)):
-            if Ys[yNdx] == L[lNdx][1]:        
-                Mu = Mu + Wl[lNdx]*l[lNdx][0].values()
+            if Ys[yNdx] == L[lNdx][1]:
+                X = np.matrix(L[lNdx][0].values())       
+                Mu = Mu + Wl[lNdx]*effW*X
         Mu = Mu/Ycounts[yNdx]
 
         for lNdx in range(len(L)):
             if Ys[yNdx] == L[lNdx][1]:        
-                Sigma = Sigma + (Wl[lNdx]*L[lNdx][0].values()-Mu).T*(Wl[lNdx]*L[lNdx][0].values()-Mu)
+                X = np.matrix(L[lNdx][0].values())       
+                Sigma = Sigma + (Wl[lNdx]*effW*X-Mu).T*(Wl[lNdx]*effW*X-Mu)
         Sigma = Sigma/Ycounts[yNdx]
 
         Mus.append(Mu)
         Sigmas.append(Sigma)
 
+
     l = Ycounts[0]+Ycounts[1]
     Pi = [Ycounts[0]/l,Ycounts[1]/l]
+
 
     ndx = -1
     diff = None
     while ndx < maxIter and threshold > diff:
         if maxIter>0:
             ndx=ndx+1
-
+        
         #E step
-        gammas = matrix(zeros(len(U),len(Ys)))
+        gammas = np.matrix(np.zeros([len(U),len(Ys)]))
+
         for uNdx in range(len(U)):
-            for yNdx in len(Range(Ys)):
-                gammas[uNdx][yNdx] = Pi[yNdx]*Normal(Mus[yNdx],Sigmas[yNdx],U[uNdx][0].values())
-            for yNdx in len(Range(Ys)):
-                gammas[uNdx][yNdx] = gammas[uNdx][yNdx]/( gammas[uNdx][1]+ gammas[uNdx][2]) 
-             
+            for yNdx in range(len(Ys)):
+                gammas[uNdx,yNdx] = Pi[yNdx]*Normal(Mus[yNdx],Sigmas[yNdx],U[uNdx][0].values())
+            denom = gammas[uNdx,0]+ gammas[uNdx,1]
+            for yNdx in range(len(Ys)):
+                gammas[uNdx,yNdx] = gammas[uNdx,yNdx]/(denom) 
         #M step
         Ljs = []
-        for yNdx in range(Ys):
-            Lj = Ycount[yNdx]
+        for yNdx in range(len(Ys)):
+            effW = Ycounts[yNdx]/Wlcounts[yNdx]
+            Lj = Ycounts[yNdx]
             for uNdx in range(len(U)):
-                Lj = Lj + gammas[uNdx][yNdx]
-            Mu = mat(zeros(len(L[0][0].keys())))
+                Lj = Lj + gammas[uNdx,yNdx]
+            Mu = np.mat(np.zeros(len(L[0][0].keys())))
             for lNdx in range(len(L)):
                 if Ys[yNdx] == L[lNdx][1]:        
-                    Mu = Mu + Wl[lNdx]*l[lNdx][0].values()
+                    X = np.matrix(L[lNdx][0].values())       
+                    Mu = Mu + Wl[lNdx]*effW*X
  
             for uNdx in range(len(U)):
-                Mu = Mu + gammas[uNdx][yNdx]*Wu[uNdx]*U[uNdx][0].values()
+                X = np.matrix(U[uNdx][0].values())       
+                Mu = Mu + gammas[uNdx,yNdx]*(Wu[uNdx]*len(U))*X
 
             Mu = Mu/Lj
 
-            Sigma=mat(zeros([len(L[0][0].keys()),len(L[0][0].keys())]))
+            Sigma=np.mat(np.zeros([len(L[0][0].keys()),len(L[0][0].keys())]))
             for lNdx in range(len(L)):
-                if Ys[yNdx] == L[lNdx][1]:        
-                    Sigma = Sigma + (Wl[lNdx]*L[lNdx][0].values()-Mu).T*(Wl[lNdx]*L[lNdx][0].values()-Mu)
+                if Ys[yNdx] == L[lNdx][1]:
+                    X = np.matrix(L[lNdx][0].values())           
+                    Sigma = Sigma + (Wl[lNdx]*effW*X-Mu).T*(Wl[lNdx]*effW*X-Mu)
  
             for uNdx in range(len(U)):
-                Sigma = Sigma + gammas[uNdx][yNdx]*(Wu[uNdx]*U[uNdx][0].values()-Mu).T*(Wu[uNdx]*U[uNdx][0].values()-Mu)
+                X = np.matrix(U[uNdx][0].values())       
+                Sigma = Sigma + gammas[uNdx,yNdx]*(len(U)*Wu[uNdx]*X-Mu).T*(len(U)*Wu[uNdx]*X-Mu)
             Sigma = Sigma/Lj
 
             Pi[yNdx] = Lj/float(len(L) + len(U))
             Sigmas[yNdx] = Sigma
             Mus[yNdx] = Mu
+
+        #need to calculate difference in log likelihood
+
     return gammas.tolist()
            
         
+
+
+
+
+
+L=[[{1:1},-1.0],[{1:0},-1.0],[{1:2},1.0],[{1:3},1.0]]
+U=[[{1:1.5},None]]
+Wl=[.25,.25,.25,.25]
+Wu=[1]
+Ys=[-1.0,1.0]
+
+
+x=EM(L,U,Wl,Wu,Ys,5,.1)
+print x
+
+
 
 
 
